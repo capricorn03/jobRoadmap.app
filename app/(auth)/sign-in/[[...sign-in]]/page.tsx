5ac6c1f33a9bac112369eca79a633a1c8240cc17
abhi <aasusgod@gmail.com>
2024-05-08 06:42:06 +0530

    chore: Update sign-in page layout and background image

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,27 @@
 import { SignIn } from "@clerk/nextjs";
+import { Fan } from "lucide-react";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div
+      style={{
+        backgroundImage: `url('/bgImage.jpg')`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="flex justify-center">
+        <div className="flex flex-col items-center">
+          <div className="flex justify-center ">
+            <Fan className="w-20 h-20" />
+          </div>
+          <div className="flex justify-center">
+            <p className="font-bold text-lg text-color-">Job Recommendation App</p>
+          </div>
+          <div className="w-50 mb-14 md:flex">
+            <SignIn />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
